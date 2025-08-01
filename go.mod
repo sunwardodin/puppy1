ae7c16ef2494b742cfdbcbfef3bacbaaece5daeb
Drew <drew@Drews-MacBook-Pro.local>
2025-08-01 08:52:16 -0600

    fourth

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sunwardodin/puppy1
 
 go 1.24.5
+
+require github.com/sunwardodin/dog1 v0.0.0-20250801142022-c6bb2831c163 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/sunwardodin/dog1"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog1.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog1.WhenGrownUp(Barks())
 }
